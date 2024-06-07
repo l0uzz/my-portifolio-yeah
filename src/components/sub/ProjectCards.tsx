6000996cf614d7309b4e8fb1671676db83e85dff
L0uzzDev <l0uzzdev@gmail.com>
2024-06-07 14:54:00 -0300

    Create a Projects Component and add to page
    Add Png of My Web Portifolio

---
+++ src/components/sub/ProjectCards.tsx
@@ -0,0 +1,28 @@
+import Image from 'next/image';
+import React from 'react'
+
+interface Props {
+  src: string;
+  title: string;
+  description: string
+}
+
+const ProjectsCards = ({src, title, description} : Props) => {
+  return (
+    <div className='relative overflow-hidden rounded-lg shadow-lg border border-[#2A0E61]'>
+        <Image
+        src={src}
+        alt={title}
+        width={1000}
+        height={1000}
+        className='w-full object-contain'
+        />
+        <div className='relative p-4'>
+          <h1 className='text-2x1 font-semibold text-white'>{title}</h1>
+          <p className='mt-2 text-gray-300'>{description}</p>
+        </div>
+    </div>
+  )
+}
+
+export default ProjectsCards
